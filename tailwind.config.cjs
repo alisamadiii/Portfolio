287d58fc66ef1337980be1917a4edb46758af225
Ali Reza <webdeve1083@gmail.com>
2023-01-09 07:19:47 +0800

    Added the images for this project and downloaded the 'react-intersection-observer'

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
+    colors: {
+      primary: "#00B7C2",
+      secondary: "#052032",
+      background: "#000000",
+    },
     extend: {},
   },
   plugins: [],
