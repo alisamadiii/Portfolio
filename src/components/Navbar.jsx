86f338234ec0e49b90a7a4984f0571dbcc8d42f6
Ali Reza <webdeve1083@gmail.com>
2023-01-09 10:15:40 +0800

    Done with the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Logo from "../../public/Logo.png";
 import { AiFillGithub, AiFillTwitterCircle } from "react-icons/ai";
+import { RiMenu3Fill } from "react-icons/ri";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="w-full h-16 border-b-2 border-light-20">
-      <div className="w-full h-full max-w-[1536px] mx-auto px-24 flex justify-between items-center">
+    <div className="z-50 w-full h-16 bg-dark border-b-2 border-light-20">
+      <div className="w-full h-full max-w-[1536px] mx-auto px-4 md:px-16 lg:px-24 flex justify-between items-center">
         <img src={Logo} width="34px" height="34px" alt="" />
 
-        <ul className="flex items-center gap-8 text-link">
-          <a href="#" className="hover:text-light">
+        <ul
+          className={`flex items-center gap-8 text-link ${
+            isOpen == true ? "navbar__active" : ""
+          }`}
+          onClick={() => setIsOpen(!isOpen)}
+          id="responsive__navbar">
+          <a
+            href="#"
+            className="w-full hover:bg-[#222222] md:hover:bg-opacity-0 py-2 px-4 md:p-0 rounded-md hover:text-light"
+            id="link">
             About
           </a>
-          <a href="#" className="hover:text-light">
+          <a
+            href="#"
+            className="w-full hover:bg-[#222222] md:hover:bg-opacity-0 py-2 px-4 md:p-0 rounded-md hover:text-light"
+            id="link">
             Projects
           </a>
-          <a href="#" className="hover:text-light">
+          <a
+            href="#"
+            className="w-full hover:bg-[#222222] md:hover:bg-opacity-0 py-2 px-4 md:p-0 rounded-md hover:text-light"
+            id="link">
             Contents
           </a>
-          <li className="flex gap-4 text-2xl">
-            <a href="#" className="hover:text-light">
+          <li className="flex gap-2 md:gap-4 text-3xl md:text-2xl mx-auto md:mx-0 mt-8 md:mt-0">
+            <a
+              href="#"
+              className="w-full hover:bg-[#222222] md:hover:bg-opacity-0 py-2 px-4 md:p-0 rounded-md hover:text-light"
+              id="link">
               <AiFillGithub />
             </a>
-            <a href="#" className="hover:text-light">
+            <a
+              href="#"
+              className="w-full hover:bg-[#222222] md:hover:bg-opacity-0 py-2 px-4 md:p-0 rounded-md hover:text-light"
+              id="link">
               <AiFillTwitterCircle />
             </a>
           </li>
         </ul>
+
+        <RiMenu3Fill
+          className="text-2xl block md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        />
       </div>
     </div>
   );
