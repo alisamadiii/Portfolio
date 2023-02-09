12c247541b998cd9731ad35355bf8148350868fe
Ali Reza <webdeve1083@gmail.com>
2023-02-10 05:28:21 +0800

    Changed website name, Changed the logo, added some animation on scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Home, About, Projects, Skills, Contact } from "./container";
 
 function App() {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden snap-y snap-mandatory snap-center">
       <Navbar />
       <Home />
       <Skills />
--- src/container/About.jsx
+++ src/container/About.jsx
@@ -1,46 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useInView } from "react-intersection-observer";
+import React from "react";
+import { motion } from "framer-motion";
 
 import AboutIMG from "../assets/image_about.png";
 import SectionGradient from "../components/Section-Gradient";
 
 function About() {
-  const [active, setActive] = useState(false);
-  const { ref: text, inView: isActive } = useInView();
-
-  useEffect(() => {
-    isActive == true && setActive(true);
-  }, [isActive]);
-
-  console.log(active);
-
   return (
     <div
-      ref={text}
       id="about"
       className="relative h-auto lg:h-screen 2xl:h-auto py-12 lg:py-0 2xl:py-24 flex justify-center px-4 lg:px-40 overflow-hidden">
       <SectionGradient />
       <div className="w-full max-w-[1536px] grid lg:grid-cols-2 items-center">
-        <div>
+        <motion.div
+          initial={{ opacity: 0, y: 200 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}>
           <h3
-            ref={text}
-            className={`text-2xl md:text-4xl lg:text-5xl font-bold mb-2 md:mb-6 duration-1000 ${
-              active ? "opacity-1 translate-x-0" : "opacity-0 -translate-x-14"
-            }`}>
+            className={`text-2xl md:text-4xl lg:text-5xl font-bold mb-2 md:mb-6 duration-1000`}>
             About
           </h3>
           <p
-            className={`text-sm md:text-lg lg:text-2xl space-grotesk duration-1000 delay-75 ${
-              active ? "opacity-1 translate-x-0" : "opacity-0 -translate-x-14"
-            }`}>
+            className={`text-sm md:text-lg lg:text-2xl space-grotesk duration-1000 delay-75`}>
             Hi, my name is Ali Reza and I am a web developer with over 2 years
             of experience in the field. I specialize in front-end development
             and have a strong background in ReactJS. I am always looking to
             learn and grow as a developer, and I am excited to work on new and
             challenging projects
           </p>
-        </div>
-        <img src={AboutIMG} className="w-[466px] mx-auto" alt="" />
+        </motion.div>
+        <motion.img
+          initial={{ opacity: 0, y: 200 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
+          src={AboutIMG}
+          className="w-[466px] mx-auto"
+          alt=""
+        />
       </div>
     </div>
   );
