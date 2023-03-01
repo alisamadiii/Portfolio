2acceeb32f41afb3b30a02136eb60fda3faf7608
Ali Reza <webdeve1083@gmail.com>
2023-03-01 13:17:57 +0800

    Added my time zone

--- components/Layouts.tsx
+++ components/Layouts.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
+const { DateTime } = require("luxon");
 
 import MyImage from "@/public/My Image.png";
 import Image from "next/image";
@@ -17,11 +18,28 @@ const domine = Domine({
 });
 
 export default function Layouts({ children }: Props) {
+  const [myTime, setMyTime] = useState("");
+
+  useEffect(() => {
+    const TimeNow = () => {
+      const now = DateTime.now().setZone("Asia/Makassar");
+      const { hour, minute, second } = now.c;
+      setMyTime(`${hour}:${minute}:${second}`);
+    };
+
+    TimeNow();
+
+    setInterval(() => TimeNow(), 1000);
+  }, []);
+
   return (
     <div
       className={`${domine.className} w-full max-w-[948px] mx-auto p-4 text-black`}
     >
       <div>
+        <p className="text-sm md:text-base opacity-70">
+          {myTime} - Asia/Makassar
+        </p>
         <Image
           src={MyImage}
           width={154}
