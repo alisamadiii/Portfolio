a089a2638f7ea1cbe71ffe3571b877746b6d43c0
Ali Reza <webdeve1083@gmail.com>
2023-03-01 17:21:54 +0800

    Added my first blog...I will add the components to make them beautiful

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,15 +1,60 @@
 import HeadTag from "@/components/Head";
+import { fileNames, pathFiles } from "@/utils/MDXFiles";
+import path from "path";
 import React from "react";
+import fs from "fs";
+import matter from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import rehypeHighlight from "rehype-highlight";
+import rehypeCodeTitles from "rehype-code-titles";
 
-type Props = {};
+type Props = {
+  mdxSource: any;
+};
 
-export default function Slug({}: Props) {
+export default function Slug({ mdxSource }: Props) {
   return (
     <>
       <HeadTag title="Testimonial" />
       <div className="mt-6">
-        <h1 className="text-2xl font-bold text-center">coming soon...</h1>
+        <MDXRemote {...mdxSource}></MDXRemote>
       </div>
     </>
   );
 }
+
+export const getStaticProps = async ({ params }: any) => {
+  const { slug } = params;
+
+  const filePath = path.join(pathFiles, `${slug}.mdx`);
+  const readFile = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(readFile);
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      rehypePlugins: [rehypeCodeTitles, rehypeHighlight],
+    },
+  });
+
+  return {
+    props: {
+      data,
+      mdxSource,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  const blogsPath = fileNames.map((slug) => {
+    return {
+      params: {
+        slug: slug.replace(".mdx", ""),
+      },
+    };
+  });
+
+  return {
+    paths: blogsPath,
+    fallback: false,
+  };
+};
