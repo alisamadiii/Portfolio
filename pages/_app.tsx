27c8dcfef1670c686268345230f1f2cf8458912e
Ali Reza <webdeve1083@gmail.com>
2023-03-09 09:20:30 +0800

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import Layouts from "@/components/Layouts";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <Layouts>
       <AnimatePresence mode="wait">
         <Component {...pageProps} key={router.pathname} />
+        <Analytics />
       </AnimatePresence>
     </Layouts>
   );
