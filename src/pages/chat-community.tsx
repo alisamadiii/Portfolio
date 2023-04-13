5ec15824db7deb3edb381269ba2dfa79cdb6a5ec
Ali Reza <webdeve1083@gmail.com>
2023-04-14 02:43:01 +0800

    Added a loader element for comments

--- src/pages/chat-community.tsx
+++ src/pages/chat-community.tsx
@@ -91,10 +91,27 @@ export default function Chat_Community({}: Props) {
           ref={listComments}
           className="flex flex-col w-full gap-3 p-4 overflow-y-auto bg-light-blue-2 rounded-xl h-96"
         >
-          {comments &&
+          {comments ? (
             comments.map((comment) => (
               <Comment key={comment.id} comment={comment} />
-            ))}
+            ))
+          ) : (
+            <div className="self-center">
+              <svg
+                fill="none"
+                className="w-16 text-primary h-1w-16 animate-spin"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  clip-rule="evenodd"
+                  d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                />
+              </svg>
+            </div>
+          )}
         </div>
         <form onSubmit={submitHandler} className="w-full gap-2 mt-auto">
           <div className="flex items-center p-2 border-2 rounded-md border-primary">
