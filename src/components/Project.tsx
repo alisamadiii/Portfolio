fee1b76a597053615dc4d125e67b284e90dbae4b
Ali Reza <webdeve1083@gmail.com>
2023-04-27 19:57:13 +0800

    Added a new functionality on reviewing each projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,6 +5,7 @@ import { ProjectType } from "@/contents/Projects";
 import DropDown_List from "./DropDown_List";
 import { AnimatePresence, motion } from "framer-motion";
 import Review_Project from "./Review_Project";
+import Image from "next/image";
 
 type Props = {
   project: ProjectType;
@@ -16,40 +17,25 @@ export default function Project({ project }: Props) {
 
   return (
     <>
-      <div
-        className="relative px-4 py-6 bg-white rounded-xl shadow-container"
+      <motion.div
+        layoutId={project.id.toString()}
+        // transition={{ delay: 1 }}
+        className="px-4 py-6 bg-white rounded-xl shadow-container"
         onMouseEnter={() => setData(project)}
         onMouseLeave={() => setData(null)}
       >
         <div className="flex items-center justify-between text-2xl font-bold">
           <h3>{project.name}</h3>
           <div>
-            <span onClick={() => setIsOpen(!isOpen)} className="inline-block">
+            <span onClick={() => setIsOpen(true)} className="inline-block">
               <BiDotsVerticalRounded />
             </span>
-            <AnimatePresence>
-              {isOpen && (
-                <>
-                  <DropDown_List
-                    data={project.links}
-                    className="absolute hidden w-48 translate-y-2 md:block right-4"
-                    targetLink={true}
-                  />
-                  <Review_Project
-                    key={project.id}
-                    data={project.links}
-                    image={project.image}
-                    setIsOpen={setIsOpen}
-                  />
-                </>
-              )}
-            </AnimatePresence>
           </div>
         </div>
         <p className="mt-2 font-medium">{project.description}</p>
-      </div>
-      <AnimatePresence>
-        {data && <Project_Image image={data.image} />}
+      </motion.div>
+      <AnimatePresence mode="wait">
+        {isOpen && <Each_Projects project={project} setIsOpen={setIsOpen} />}
       </AnimatePresence>
     </>
   );
@@ -80,3 +66,46 @@ export function Project_Image({ image }: Image_Props) {
     </motion.div>
   );
 }
+
+type EachProjectsProps = {
+  project: ProjectType;
+  setIsOpen: (a: boolean) => void;
+};
+
+export const Each_Projects = ({ project, setIsOpen }: EachProjectsProps) => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-between font-bold isolate">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="absolute top-0 left-0 w-full h-full bg-white/50 -z-10"
+        onClick={() => setIsOpen(false)}
+      ></motion.div>
+      <motion.div
+        layoutId={project.id.toString()}
+        layout
+        className="p-4 mx-auto bg-white shadow-lg rounded-xl"
+        style={{ width: "100%", maxWidth: "700px" }}
+      >
+        <h3 className="text-2xl">{project.name}</h3>
+        <p className="mt-2 font-medium">{project.description}</p>
+        <motion.div
+          // initial={{ height: "0" }}
+          // animate={{ height: "auto" }}
+          // exit={{ height: "0" }}
+          // transition={{ duration: 0.2, type: "tween", delay: 0.2 }}
+          className="overflow-hidden"
+        >
+          <Image
+            src={project.image}
+            width={1000}
+            height={1000}
+            alt=""
+            className="object-cover aspect-video"
+          />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
