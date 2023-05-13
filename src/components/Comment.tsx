bfd44d6b721f0d39bb238aa5cb46b37a059c0e35
Ali Reza <webdeve1083@gmail.com>
2023-05-13 13:04:57 +0800

    Using absolute position icons in each comments

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -12,7 +12,8 @@ import EditComment from "./EditComment";
 import { createToast } from "vercel-toast";
 
 // Icons
-import { BsQuestionDiamondFill } from "react-icons/bs";
+import { BsQuestionDiamondFill, BsThreeDotsVertical } from "react-icons/bs";
+import { FcManager } from "react-icons/fc";
 
 type Props = {
   comment: COMMENT;
@@ -22,6 +23,7 @@ type Props = {
 export default function Comment({ comment, setIsNotSigned }: Props) {
   const { currentUser } = useContext(User_Context);
   const [editOpen, setEditOpen] = useState<boolean>(false);
+  const [isChangeComment, setIsChangeComment] = useState<boolean>(false);
 
   const deletingComment = async (id: string) => {
     const docRef = doc(db, "comments", id);
@@ -77,9 +79,13 @@ export default function Comment({ comment, setIsNotSigned }: Props) {
         initial={{ opacity: 0, x: -20 }}
         animate={{ opacity: 1, x: 0 }}
         layout="position"
-        className={`flex items-center gap-4 px-4 py-2 hover:bg-dark-blue-2/10 ${
+        className={`relative flex items-center gap-4 px-4 py-2 hover:bg-dark-blue-2/5 isolate ${
           currentUser?.uid == comment.userId && "bg-dark-blue-2/10"
-        } ${comment.chatType == "question" && "!bg-secondary/20"}`}
+        } ${
+          comment.chatType == "question" && "!bg-secondary/20"
+        } before:absolute before:inset-0 before:bg-white before:duration-200 before:-z-10 ${
+          isChangeComment ? "before:opacity-100" : "before:opacity-0"
+        }`}
       >
         {/* <div className="relative self-start w-8 h-8">
           <Image
@@ -110,7 +116,12 @@ export default function Comment({ comment, setIsNotSigned }: Props) {
           </div>
         )}
         <div className="grow">
-          <div>
+          <div
+            className={`${
+              comment.userId === process.env.NEXT_PUBLIC_OWNER && "flex gap-1"
+            }`}
+          >
+            {comment.userId === process.env.NEXT_PUBLIC_OWNER && <FcManager />}
             <small className="italic opacity-60">{comment.name}</small>
             {/* <small className="text-xs italic opacity-60">
               {toDateTime(comment.createdAt.seconds)}
@@ -119,41 +130,77 @@ export default function Comment({ comment, setIsNotSigned }: Props) {
           <p className="text-sm md:text-base">{comment.message}</p>
           {/* <small>{timeFormat(comment.createdAt.seconds)}</small> */}
         </div>
-        <div className="flex items-center gap-2">
-          <p
-            className={`${
-              findingComment && "text-blue-600"
-            } flex bg-blue-600/10 p-1 rounded-md cursor-pointer gap-1 active:scale-95 duration-100`}
-            onClick={() => updatingLikes(comment)}
-          >
-            <AiFillLike />
-            <span className="text-xs">{comment.likes.length}</span>
-          </p>
-          {currentUser?.uid === comment.userId && (
-            <>
-              <p
-                onClick={() => setEditOpen(!editOpen)}
-                className="p-1 text-yellow-500 rounded-md cursor-pointer bg-yellow-500/10"
-              >
-                <AiFillEdit />
-              </p>
-              <p
-                onClick={() => deletingComment(comment.id)}
-                className="p-1 text-red-700 rounded-md cursor-pointer bg-red-700/10"
-              >
-                <BsFillTrash3Fill />
-              </p>
-            </>
-          )}
-          {currentUser?.uid === process.env.NEXT_PUBLIC_OWNER && (
-            <p
-              onClick={() => deletingComment(comment.id)}
-              className="p-1 text-green-700 rounded-md cursor-pointer bg-green-700/10"
+        <p
+          className={`${
+            findingComment && "text-blue-600"
+          } flex bg-blue-600/10 p-1 rounded-md cursor-pointer gap-1 active:scale-95 duration-100`}
+          onClick={() => updatingLikes(comment)}
+        >
+          <AiFillLike />
+          <span className="text-xs">{comment.likes.length}</span>
+        </p>
+
+        <AnimatePresence>
+          {isChangeComment && (
+            <motion.div
+              initial={{ scale: 0.9, y: 5 }}
+              animate={{ scale: 1, y: 0 }}
+              exit={{ scale: 0.9, y: 5, opacity: 0 }}
+              className="absolute flex items-center gap-2 p-2 rounded-md shadow-lg right-4 bg-light-blue -top-6"
             >
-              <BsFillTrash3Fill />
-            </p>
+              {currentUser?.uid === comment.userId && (
+                <>
+                  <p
+                    onClick={() => setEditOpen(!editOpen)}
+                    className="p-1 text-yellow-500 rounded-md cursor-pointer bg-yellow-500/10"
+                  >
+                    <AiFillEdit />
+                  </p>
+                  <p
+                    onClick={() => deletingComment(comment.id)}
+                    className="p-1 text-red-700 rounded-md cursor-pointer bg-red-700/10"
+                  >
+                    <BsFillTrash3Fill />
+                  </p>
+                </>
+              )}
+              {currentUser?.uid === process.env.NEXT_PUBLIC_OWNER && (
+                <p
+                  onClick={() => deletingComment(comment.id)}
+                  className="p-1 text-green-700 rounded-md cursor-pointer bg-green-700/10"
+                >
+                  <BsFillTrash3Fill />
+                </p>
+              )}
+            </motion.div>
           )}
-        </div>
+        </AnimatePresence>
+
+        {currentUser?.uid == comment.userId ? (
+          <div
+            className={`p-1 rounded-md ${
+              isChangeComment
+                ? "bg-dark-blue text-light-blue"
+                : "bg-dark-blue/10 text-dark-blue"
+            }`}
+            onClick={() => setIsChangeComment(!isChangeComment)}
+          >
+            <BsThreeDotsVertical />
+          </div>
+        ) : (
+          currentUser?.uid === process.env.NEXT_PUBLIC_OWNER && (
+            <div
+              className={`p-1 rounded-md ${
+                isChangeComment
+                  ? "bg-dark-blue text-light-blue"
+                  : "bg-dark-blue/10 text-dark-blue"
+              }`}
+              onClick={() => setIsChangeComment(!isChangeComment)}
+            >
+              <BsThreeDotsVertical />
+            </div>
+          )
+        )}
       </motion.div>
 
       <AnimatePresence>
