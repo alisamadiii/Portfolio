0b5e93b5f487006f572a4b93e794b74213c3cc55
Ali Reza <webdeve1083@gmail.com>
2023-06-11 09:37:45 +0800

    Done with the Navbar's Teset

--- src/__test__/Navbar.test.tsx
+++ src/__test__/Navbar.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import Navbar from "../components/Navbar";
 
-test("navbar", () => {
-  render(<Navbar />);
+describe("Navbar", () => {
+  test("Navbar must contains logo, nav-links, button", () => {
+    render(<Navbar />);
 
-  const logo = screen.getByAltText("logo");
+    const logo = screen.getByAltText("logo");
+    const LinkAbout = screen.getByRole("link", { name: /about/i });
+    const LinkProject = screen.getByRole("link", { name: /project/i });
+    const LinkProducts = screen.getByRole("link", { name: /products/i });
+    const LinkBlogs = screen.getByRole("link", { name: /blogs/i });
+    const button = screen.getByText("Chat Now");
 
-  expect(logo).toBeInTheDocument();
+    expect(
+      logo && button && LinkAbout && LinkProject && LinkProducts && LinkBlogs
+    ).toBeInTheDocument();
+  });
 });
