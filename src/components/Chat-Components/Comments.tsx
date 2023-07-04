75aa2a5b365fc0df963c11b3978b4681701e2400
Ali Reza <webdeve1083@gmail.com>
2023-07-04 13:08:05 +0800

    Added images for all the comments

--- src/components/Chat-Components/Comments.tsx
+++ src/components/Chat-Components/Comments.tsx
@@ -50,6 +50,8 @@ export default function Comments({}: Props) {
     }
   }
 
+  let previousUser: any = null;
+
   return (
     <ul
       id="chat"
@@ -57,60 +59,68 @@ export default function Comments({}: Props) {
       className="flex flex-col items-start gap-2 p-2 overflow-auto grow h-96"
     >
       {comments &&
-        comments.map((comment) => (
-          <motion.li
-            key={comment.id}
-            layout
-            className={`group flex items-center gap-2 ${
-              comment.userId == currentUser?.uid
-                ? "self-end flex-row-reverse"
-                : ""
-            }`}
-          >
-            {/* Image */}
-            {currentUser?.uid == comment.userId && (
-              <Image
-                src={currentUser.photoURL}
-                width={40}
-                height={40}
-                alt={currentUser.displayName}
-                className="self-start -translate-y-3 rounded-full w-7"
-              />
-            )}
-            {/* Comment */}
-            <div
-              className={`group relative text-white p-2 rounded-xl max-w-[300px] min-w-[100px] duration-150 ${
+        comments.map((comment) => {
+          const isSameUser = comment.userId === previousUser;
+          previousUser = comment.userId;
+
+          return (
+            <motion.li
+              key={comment.id}
+              layout
+              className={`group flex items-center gap-2 ${
                 comment.userId == currentUser?.uid
-                  ? `self-end rounded-tr-none ${
-                      isCtrlPressed
-                        ? "bg-red-600 cursor-pointer"
-                        : "bg-[#00B871]"
-                    }`
-                  : "rounded-tl-none bg-primary"
-              } ${
-                process.env.NEXT_PUBLIC_OWNER == comment?.userId &&
-                "!bg-orange-600"
+                  ? "self-end flex-row-reverse"
+                  : ""
               }`}
-              onClick={() => {
-                isCtrlPressed &&
-                  (currentUser?.uid == comment.userId ||
-                    process.env.NEXT_PUBLIC_OWNER == currentUser?.uid) &&
-                  deletingComment(comment.id);
-              }}
             >
-              <span>{comment.message}</span>
-              <span className="float-right mt-2 ml-2 text-xs opacity-80">
-                {currentUser?.uid !== comment.userId && `${comment.name} - `}
-                {comment.createdAt &&
-                  convertTimestampToDateTime(comment.createdAt.seconds)}
-              </span>
-            </div>
-            {/* Edit */}
-            <div className="duration-200 opacity-0 group-hover:opacity-100">
-              <Like comment={comment} />
-            </div>
-          </motion.li>
-        ))}
+              {/* Image */}
+              {!isSameUser && (
+                <Image
+                  src={comment.image}
+                  width={40}
+                  height={40}
+                  alt={comment.name}
+                  className="self-start -translate-y-3 rounded-full w-7"
+                />
+              )}
+              {isSameUser && <div className="w-7" />}
+              {/* Comment */}
+              <div
+                className={`group relative text-white p-2 rounded-xl max-w-[300px] min-w-[100px] duration-150 ${
+                  comment.userId == currentUser?.uid
+                    ? `self-end ${
+                        isCtrlPressed
+                          ? "bg-red-600 cursor-pointer"
+                          : "bg-[#00B871]"
+                      } ${!isSameUser ? "rounded-tr-none" : ""}`
+                    : `bg-primary ${!isSameUser ? "rounded-tl-none" : ""}`
+                } ${
+                  process.env.NEXT_PUBLIC_OWNER == comment?.userId &&
+                  "!bg-orange-600"
+                } `}
+                onClick={() => {
+                  isCtrlPressed &&
+                    (currentUser?.uid == comment.userId ||
+                      process.env.NEXT_PUBLIC_OWNER == currentUser?.uid) &&
+                    deletingComment(comment.id);
+                }}
+              >
+                <span>{comment.message}</span>
+                <span className="float-right mt-2 ml-2 text-xs opacity-80">
+                  {currentUser?.uid !== comment.userId &&
+                    !isSameUser &&
+                    `${comment.name} - `}
+                  {comment.createdAt &&
+                    convertTimestampToDateTime(comment.createdAt.seconds)}
+                </span>
+              </div>
+              {/* Edit */}
+              <div className="duration-200 opacity-0 group-hover:opacity-100">
+                <Like comment={comment} />
+              </div>
+            </motion.li>
+          );
+        })}
     </ul>
   );
 }
