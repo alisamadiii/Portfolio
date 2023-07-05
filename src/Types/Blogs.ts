a827996fe9262e3771e663c59746f428dbb64515
Ali Reza <webdeve1083@gmail.com>
2023-07-05 14:48:11 +0800

    Deleted some old files

--- src/Types/Blogs.ts
+++
@@ -1,9 +0,0 @@
-export type DATA_Type = {
-  blog: number;
-  title: string;
-  description: string;
-  tags: string[];
-  image: string;
-  author: string;
-  createdAt: string;
-};
---
+++ src/components/Chat-Components/EditComment.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+export default function EditComment({}: Props) {
+  //   const submitHandler = async (e: any) => {
+  //     e.preventDefault();
+  //     const docRef = doc(db, "comments", comment.id);
+  //     updateDoc(docRef, {
+  //       message: update,
+  //     });
+  //     setEditOpen(false);
+  //   };
+  return <div>EditComment</div>;
+}
