015558e123a8337aec661f0e1b86b8df7e7956b9
Ali Reza <webdeve1083@gmail.com>
2023-08-05 16:16:26 +0800

    The files are restart

--- sponsor/pages/_document.tsx
+++ sponsor/pages/_document.tsx
@@ -3,11 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Sponsorship - @Ali_Developer05</title>
-        <link rel="shortcut icon" href="./logo.jpg" type="image/x-icon" />
-      </Head>
-      <body className="bg-light-blue text-dark-blue">
+      <Head />
+      <body className="bg-white text-secondary">
         <Main />
         <NextScript />
       </body>
--- sponsor/tailwind.config.js
+++ sponsor/tailwind.config.js
@@ -6,20 +6,18 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#6747EB",
-        secondary: "#FF9C45",
-        "dark-blue": "#2C2F5A",
-        "dark-blue-2": "#696C9A",
-        "light-blue": "#F1F5FF",
-        "light-blue-2": "#E7EDFB",
-        twitter: "#00acee",
-      },
-      boxShadow: {
-        container: "0 10px 65px rgba(0, 0, 0, .09)",
-      },
+    colors: {
+      primary: "#1D9BF0",
+      secondary: "#0F1419",
+      "primary-hover": "#1A8CD8",
+      "font-2": "#536471",
+      "button-hover": "#E7E7E8",
+      white: "#fff",
+      black: "#000",
+      test: "#ff0000",
     },
+    fontSize: {},
+    extend: {},
   },
   plugins: [],
 };
