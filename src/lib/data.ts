f3c5f426e355c84ee9ec4ccb6dbd6cc1332c7b94
Ali Reza <webdeve1083@gmail.com>
2023-09-20 11:15:28 +0800

    added good description for my experience section

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -17,7 +17,7 @@ const Experience = [
     title: "Content Creations",
     subtitle: "X",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "I began creating animated content in January 2023, and within one month, I gained 10k followers. My content has been well-received by many people, and I continue to improve it every day.",
     date: "2020",
     icon: React.createElement(FaEarthAmericas),
   },
@@ -26,7 +26,7 @@ const Experience = [
     title: "Front-End Developer",
     subtitle: "HTML, CSS & JS",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "Like everyone else, I began my journey by learning HTML and CSS. I completed numerous projects using these technologies. Once I felt comfortable with HTML and CSS, I decided to delve into JavaScript. At first, I questioned the purpose of learning JavaScript, but eventually, I discovered its significance.",
     date: "2020 - 2022",
     icon: React.createElement(BsVectorPen),
   },
@@ -35,7 +35,7 @@ const Experience = [
     title: "Front-End Developer",
     subtitle: "Figma, Next.js & Framer Motion",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "If I want to undertake a massive project, I need to use technologies that will assist me in its development. That's why I decided to learn CSS framework (Tailwindcss) and JavaScript library (Reactjs). I dedicate most of my time to building projects and contributing to open source, as I recognize that these are the most effective ways to improve oneself - by actively creating and collaborating on other people's projects. Additionally, I've started working with Figma because I understand that if I want to work independently or start my own company, having knowledge of UI is crucial. Thankfully, everything is going well so far. Occasionally, I may become frustrated when encountering a bug that takes time to solve, but I am confident that I will find a solution soon.",
     date: "2022 - present",
     icon: React.createElement(FaReact),
   },
