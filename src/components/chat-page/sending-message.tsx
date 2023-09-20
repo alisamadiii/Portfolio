d5987b083ce3d09d12cc7592318d062d6c5105ba
Ali Reza <webdeve1083@gmail.com>
2023-09-20 18:09:29 +0800

    Added animation when replying to a message

--- src/components/chat-page/sending-message.tsx
+++ src/components/chat-page/sending-message.tsx
@@ -1,21 +1,26 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import useMeasure from "react-use-measure";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { UseUserContext } from "@/context/User.context";
 import { Label } from "@/components/ui/label";
 import CustomIcon from "@/assets/CustomIcon";
 import { supabase } from "@/utils/supabase";
 
-import type { MessageValue } from "@/types/chat-history.t";
 import { useChatStore } from "@/context/Chat.context";
 import { Text } from "../ui/text";
+
+import { AiOutlineClose } from "react-icons/ai";
 
 type Props = {};
 
 export default function SendingMessage({}: Props) {
   const [message, setMassage] = useState("");
   const { messages, setMessages, replyId, setReplyId } = useChatStore();
+
+  const [label, { height }] = useMeasure();
 
   const { currentUser } = UseUserContext();
 
@@ -60,26 +65,40 @@ export default function SendingMessage({}: Props) {
     >
       {/* Inputs */}
       <Label
+        ref={label}
         className={`flex flex-col items-center bg-accents-1 grow rounded-xl pl-2 pr-3 py-2`}
       >
-        {replyId && (
-          <div
-            className="bg-background px-2 py-0.5 rounded relative overflow-hidden mb-2 w-full"
-            onClick={() => setReplyId(null)}
-          >
-            <div className="absolute top-0 left-0 w-[2px] h-full bg-white" />
-            <Text
-              as="h3"
-              size={10}
-              className="tracking-widest !text-white text-xxs"
+        <AnimatePresence>
+          {replyId && (
+            <motion.div
+              initial={{ height: 0, opacity: 0 }}
+              animate={{ height: "auto", opacity: 1 }}
+              exit={{ height: 0, opacity: 0 }}
+              className="w-full overflow-hidden"
+              key={replyId}
             >
-              Ali
-            </Text>
-            <Text size={10} className="leading-4 line-clamp-3">
-              {messages.find((message) => message.id == replyId)?.message}
-            </Text>
-          </div>
-        )}
+              <div className="bg-background px-2 py-0.5 rounded relative overflow-hidden mb-2 w-full">
+                <div className="absolute top-0 left-0 w-[2px] h-full bg-white" />
+                <Text
+                  as="h3"
+                  size={10}
+                  className="tracking-widest !text-white text-xxs"
+                >
+                  Ali
+                </Text>
+                <Text size={10} className="leading-4 line-clamp-3">
+                  {messages.find((message) => message.id == replyId)?.message}
+                </Text>
+                <span
+                  className="absolute cursor-pointer text-xxs top-2 right-2"
+                  onClick={() => setReplyId(null)}
+                >
+                  <AiOutlineClose />
+                </span>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
         <div className="flex items-center w-full">
           <textarea
             className="py-1 leading-5 bg-transparent outline-none resize-none grow placeholder:text-accents-6/50 max-h-20"
--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,26 +1,31 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
+import { motion } from "framer-motion";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { HTMLMotionProps } from "framer-motion";
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
+);
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+type TextProps = React.HTMLAttributes<HTMLLabelElement> &
+  VariantProps<typeof labelVariants> &
+  HTMLMotionProps<"label">;
 
-export { Label }
+const Label = React.forwardRef<HTMLLabelElement, TextProps>(
+  ({ className, ...props }, ref) => (
+    <motion.label
+      ref={ref}
+      className={cn(labelVariants(), className)}
+      {...props}
+    />
+  )
+);
+
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };
