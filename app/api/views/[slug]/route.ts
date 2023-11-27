19b8499be9a3f0e8e5f9fc64be7cd1e0d2e7831f
Ali Reza Samadi <webdeve1083@gmail.com>
2023-11-27 20:50:28 +0800

    test(page-views): page view only works on production

--- app/api/views/[slug]/route.ts
+++ app/api/views/[slug]/route.ts
@@ -3,14 +3,25 @@ import { type NextRequest } from "next/server";
 
 export async function POST(req: NextRequest, context: any) {
   const { slug } = context.params;
-  // await supabase.rpc("increment_page_view", {page_slug: req.})
-  await supabase.rpc("increment_page_view", {
-    page_slug: slug,
-  });
-  return new Response(
-    JSON.stringify({
-      message: `Successfully incremented page: ${slug}`,
-    }),
-    { status: 200 }
-  );
+
+  // Check if the environment is production
+  if (process.env.NODE_ENV === "production") {
+    await supabase.rpc("increment_page_view", {
+      page_slug: slug,
+    });
+
+    return new Response(
+      JSON.stringify({
+        message: `Successfully incremented page: ${slug}`,
+      }),
+      { status: 200 }
+    );
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: `Page view increment skipped on non-production environment for page: ${slug}`,
+      }),
+      { status: 200 }
+    );
+  }
 }
