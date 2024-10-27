9bf04e0d72393a34468f09e583ff0cd6e48260c9
Ali Samadi <alirs.dev@gmail.com>
2024-10-27 16:03:43 -0700

    fix: vercel issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${lora.variable} ${inter.variable} antialiased`}>
-        {/* <FixedImage /> */}
+        <FixedImage />
 
         {children}
 
