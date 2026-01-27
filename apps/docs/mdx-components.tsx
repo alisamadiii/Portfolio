364f914a5d1a1c0b8926993b35bbd7a0dd7ad9bb
Ali Samadi <a@alisamadii.com>
2026-01-27 10:57:54 -0800

    feat: added docs

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,9 +1,21 @@
+import { Banner } from "fumadocs-ui/components/banner";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
+import { Step, Steps } from "fumadocs-ui/components/steps";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    img: (props) => (
+      <ImageZoom
+        className="rounded-xl outline-2 -outline-offset-2 outline-black/10"
+        {...(props as any)}
+      />
+    ),
+    Step,
+    Steps,
+    Banner,
     ...components,
   };
 }
