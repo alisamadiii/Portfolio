f0c90099862a42fcef715db7ece25feac91555bf
Ali Samadi <a@alisamadii.com>
2026-01-31 19:17:31 -0800

    fix: logo color

--- packages/ui/src/icons/logo.tsx
+++ packages/ui/src/icons/logo.tsx
@@ -10,12 +10,12 @@ export const Logo = () => {
       <g clipPath="url(#clip0_3561_212)">
         <path
           d="M70.5869 82.0243H97.8874V100H70.5869V82.0243ZM46.4913 0L34.6717 20.9415H48.0666L29.2434 54.293H58.894L70.5869 33.576V57.6897H13.9329L2.11328 78.6276H15.5081L3.44588 100H33.1L45.1623 78.6276H97.8874V0H46.4913Z"
-          fill="#232323"
+          fill="currentColor"
         />
       </g>
       <defs>
         <clipPath id="clip0_3561_212">
-          <rect width="100" height="100" fill="white" />
+          <rect width="100" height="100" fill="currentColor" />
         </clipPath>
       </defs>
     </svg>
