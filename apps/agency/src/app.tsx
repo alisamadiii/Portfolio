89ef992c88d1cc6566aea062f1a98b84fc5682ca
Ali Samadi <a@alisamadii.com>
2026-04-11 14:39:25 -0700

    feat: removed old design

--- apps/agency/src/app.tsx
+++ apps/agency/src/app.tsx
@@ -1,32 +1,3 @@
-import { Navbar } from "./components/navbar";
-import { Hero } from "./components/hero";
-import { Marquee } from "./components/marquee";
-import { Stats } from "./components/stats";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Process } from "./components/process";
-import { Pricing } from "./components/pricing";
-import { CTA } from "./components/cta";
-import { Contact } from "./components/contact";
-import { Footer } from "./components/footer";
-
 export function App() {
-  return (
-    <div className="bg-bg text-text antialiased">
-      <div className="grain" />
-      <Navbar />
-      <main>
-        <Hero />
-        <Marquee />
-        <Stats />
-        <About />
-        <Services />
-        <Process />
-        <Pricing />
-        <CTA />
-        <Contact />
-        <Footer />
-      </main>
-    </div>
-  );
+  return <div className="bg-bg text-text antialiased"></div>;
 }
